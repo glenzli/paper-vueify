88ce9d0f9f046ff9f6cee1cfdc6ac0f0a9a9f300
luz-alphacode <luz_alphacode@outlook.com>
2019-04-28 21:33:29 +0800

    update demo build

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,9 @@ module.exports = {
     }
   },
   chainWebpack: config => {
-    if (process.env.VUE_APP_DEMO === 'OFF') {
-      // to get .d.ts declaration file, almost a year but the bug remains
-      // disable cache loader
+    // to get .d.ts declaration file, almost a year but the bug remains
+    // disable cache loader
+    if (process.env.NODE_ENV === 'production') {
       config.module.rule("ts").uses.delete("cache-loader");
 
       config.module.rule('ts').use('ts-loader').loader('ts-loader').tap(opts => {
@@ -22,16 +22,18 @@ module.exports = {
         return opts
       })
 
-      config.externals({
-        'arrayex': 'arrayex',
-        'direct-object': 'direct-object',
-        'paper': 'paper',
-        'paper-vueify-datatypes': 'paper-vueify-datatypes',
-        'vue': 'vue',
-        'core-js': 'core-js',
-        'vue-class-component': 'vue-class-component',
-        'vue-property-decorator': 'vue-property-decorator'
-      })
+      if (!process.env.DEMO_TARGET) {
+        config.externals({
+          'arrayex': 'arrayex',
+          'direct-object': 'direct-object',
+          'paper': 'paper',
+          'paper-vueify-datatypes': 'paper-vueify-datatypes',
+          'vue': 'vue',
+          'core-js': 'core-js',
+          'vue-class-component': 'vue-class-component',
+          'vue-property-decorator': 'vue-property-decorator'
+        })
+      }
     }
   }
 }
