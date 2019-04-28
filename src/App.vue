1f611d95c200c36c223acd5ce8d115ae6c04dd42
luz-alphacode <luz_alphacode@outlook.com>
2019-04-28 17:07:42 +0800

    update link

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ export default class App extends Vue {
   }
 
   Jump() {
-    window.location.href = './api'
+    window.location.href = 'https://luz-alphacode.github.io/paper-vueify/api/'
   }
 }
 </script>
